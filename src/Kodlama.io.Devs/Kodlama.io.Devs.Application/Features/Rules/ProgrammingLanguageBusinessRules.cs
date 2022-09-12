f27c8fd31231390fb17d8337515c4106d353b7fd
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-09-12 22:53:39 +0300

    homework application finished

---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/Rules/ProgrammingLanguageBusinessRules.cs
@@ -0,0 +1,23 @@
+﻿using Core.Persistence.Paging;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+using Core.CrossCuttingConcerns.Exceptions;
+
+namespace Kodlama.io.Devs.Application.Features.Rules
+{
+    public class ProgrammingLanguageBusinessRules
+    {
+        private readonly IProgrammingLanguagesRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageBusinessRules(IProgrammingLanguagesRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingLanguageNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(b => b.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Programming language name exists.");
+        }
+    }
+}
---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Domain/Entities/ProgrammingLanguage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Persistence.Repositories;
+
+namespace Kodlama.io.Devs.Domain.Entities
+{
+    public  class ProgrammingLanguage : Entity
+    {
+        public string Name { get; set; }
+        public ProgrammingLanguage()
+        {
+                
+        }
+
+        public ProgrammingLanguage(int id, string name) : this()
+        {
+            Id = id;
+            Name = name;
+        }
+
+    }
+}
