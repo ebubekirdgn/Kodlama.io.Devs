b32246fa04e8e11484ea027ede1d71986584692c
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-09-21 14:55:12 +0300

    Update Program.cs

--- src/Kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Program.cs
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.WebAPI/Program.cs
@@ -4,7 +4,6 @@ using Kodlama.io.Devs.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 //builder.Services.AddApplicationServices();
 //builder.Services.AddSecurityServices();
