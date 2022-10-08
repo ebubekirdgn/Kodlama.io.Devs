b30169c767ac7a6ca31a026e359c2e3d5cb58c8b
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-10-08 04:24:19 +0300

    update

--- src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/UserOperationClaims/Rules/UserOperationClaimBusinessRules.cs
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/UserOperationClaims/Rules/UserOperationClaimBusinessRules.cs
@@ -5,16 +5,30 @@ namespace Kodlama.io.Devs.Application.Features.UserOperationClaims.Rules
     public class UserOperationClaimBusinessRules
     {
         private readonly IUserOperationClaimRepository _userOperationClaimRepository;
+        private readonly IUserRepository _userRepository;
 
-        public UserOperationClaimBusinessRules(IUserOperationClaimRepository userOperationClaimRepository)
+        public UserOperationClaimBusinessRules(IUserOperationClaimRepository userOperationClaimRepository, IUserRepository userRepository)
         {
             _userOperationClaimRepository = userOperationClaimRepository;
+            _userRepository = userRepository;
         }
 
         public async Task UserOperationClaimShouldExists(int id)
         {
-            UserOperationClaim? result = await _userOperationClaimRepository.GetAsync(x => x.Id == id);
-            if (result == null) throw new BusinessException("Not exists");
+            var result = await _userOperationClaimRepository.GetAsync(x => x.Id == id);
+            if (result is null) throw new BusinessException("Role Not exists");
+        }
+
+        public async Task UserShouldExists(int id)
+        {
+            var result = await _userRepository.GetAsync(x => x.Id == id);
+            if (result is null) throw new BusinessException("User Not exists");
+        }
+
+        public void UserOperationClaimShouldExistToDelete(UserOperationClaim userOperationClaims)
+        {
+            if (userOperationClaims is null)
+                throw new BusinessException("User Role Action Does Not Exist");
         }
     }
 }
